bb15e15dcdcfd18dae9aa461fe0f2acb804c9228
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-21 11:25:21 +0300

    Update getStudentGrade.js

--- getStudentGrade.js
+++ getStudentGrade.js
@@ -1,15 +1,15 @@
 function generateGrade() {
     const marks = parseInt(document.getElementById('marks').value);
-  
-    if (isNaN(marks) || marks < 0 || marks > 100) {
-      document.getElementById('gradeOutput').textContent = 'Please enter valid marks between 0 and 100.';
-      return;
+  const stringGrades = isNaN
+    if (marks ===stringGrades || marks < 0 || marks > 100) {
+    let neMarks =  document.getElementById('gradeOutput').textContent = 'Please enter valid marks between 0 and 100.';
+      return newMarks;
     }
   
     let grade = '';
   
     if (marks > 79) {
-      grade = 'A';
+      grade = 'A'; 
     } else if (marks >= 60) {
       grade = 'B';
     } else if (marks >= 50) {
